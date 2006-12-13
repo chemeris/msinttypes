0e317678d4a14f8e074e42d0fd66f08b5bc6e60d
Alexander Chemeris <chemeris@users.noreply.github.com>
2006-12-13 12:53:52 +0000

    Error message for non-MSC compiler changed.

--- stdint.h
+++ stdint.h
@@ -30,7 +30,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef _MSC_VER // [
-#error "Use this header only with Microsoft Visual Studio 2003 or later!"
+#error "Use this header only with Microsoft Visual C++ version 7 or later!"
 #endif // _MSC_VER ]
 
 #ifndef _MSC_STDINT_H_ // [
